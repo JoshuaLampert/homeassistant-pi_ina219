c1eb2b680f0e0d5bd79c45e97876781e6a778688
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-10-03 22:45:19 +0200

    formatting

--- custom_components/pi_ina219/__init__.py
+++ custom_components/pi_ina219/__init__.py
@@ -1,4 +1,5 @@
 """The INA219 Power Monitor integration."""
+
 from __future__ import annotations
 
 import logging
@@ -17,20 +18,20 @@ PLATFORMS: list[Platform] = [Platform.SENSOR]
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up INA219 Power Monitor from a config entry."""
     _LOGGER.debug("Setting up INA219 Power Monitor integration")
-    
+
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    
+
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     _LOGGER.debug("Unloading INA219 Power Monitor integration")
-    
+
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
-    
+
     return unload_ok
